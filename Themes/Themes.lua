8766b6e55975f59be5625847a688d73e1ef2372b
Emerson Jose Boito Neto <josenetoboito@gmail.com>
2025-03-08 18:50:32 +0100

    up

---
+++ Themes/Themes.lua
@@ -0,0 +1,13 @@
+--[[
+
+Default - Default
+Amber Glow - AmberGlow
+Amethyst - Amethyst
+Bloom - Bloom
+Dark Blue - DarkBlue
+Green - Green
+Light - Light
+Ocean - Ocean
+Serenity - Serenity
+
+--]]
